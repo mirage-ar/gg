953bccd55e097f6de3c8d0ab4f69eb4da52de3c3
fiigmnt <fiigmnt@protonmail.com>
2024-01-04 11:59:09 -0500

    testing session

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,7 @@ const OPTIONS = {
       clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
       version: "2.0", // opt-in to Twitter OAuth 2.0
       profile(profile: any) {
+        console.log(profile);
         return {
           id: profile.data.id,
           name: profile.data.name,
