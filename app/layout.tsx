90c29c962a72183c366646de5e3592231516d553
fiigmnt <fiigmnt@protonmail.com>
2024-02-27 14:57:33 -0500

    removed template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,44 @@
+"use client";
+
 import React from "react";
-import type { Metadata, Viewport } from "next";
+import { PrivyProvider } from "@privy-io/react-auth";
 import MapboxMap from "@/components/map/MapboxMap";
 
 import "./globals.css";
 
-const viewport: Viewport = {
-  themeColor: '#000',
-  initialScale: 1,
-  maximumScale: 1,
-  minimumScale: 1,
-  width: 'device-width',
-  height: 'device-height',
-  userScalable: false,
-  colorScheme: 'dark',
-};
+export function generateViewport() {
+  return {
+    themeColor: "#000",
+    initialScale: 1,
+    maximumScale: 1,
+    minimumScale: 1,
+    width: "device-width",
+    height: "device-height",
+    userScalable: false,
+    colorScheme: "dark",
+  };
+}
 
-export const metadata: Metadata = {
-  title: "GG",
-  description: "GG",
-  appleWebApp: true,
-  viewport: viewport,
-  themeColor: "#000000",
-  openGraph: {
-    title: "GG",
-    description: "GG",
-    url: "https://gg.zip",
-    images: [
-      {
-        url: "https://gg.zip/og.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary",
-    title: "GG",
-    description: "GG",
-    images: [
-      {
-        url: "https://gg.zip/og.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {  
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <PrivyProvider
+          appId="clq5sknmv005vld0fk12orbq1"
+          config={{
+            // Display email and wallet as login methods
+            loginMethods: ["twitter"],
+            // Customize Privy's appearance in your app
+            appearance: {
+              theme: "dark",
+              accentColor: "#676FFF",
+              logo: "https://your-logo-url",
+            },
+          }}
+        >
+          <MapboxMap />
+          {children}
+        </PrivyProvider>
       </body>
     </html>
   );
