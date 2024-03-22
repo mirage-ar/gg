7d2535fa724f1fae4e83f24177e21333979dd105
fiigmnt <fiigmnt@protonmail.com>
2024-03-22 13:58:26 -0400

    error message

--- app/api/auth/error/page.tsx
+++ app/api/auth/error/page.tsx
@@ -9,9 +9,17 @@ const ErrorPage = () => {
   const searchParams = useSearchParams();
   const error = searchParams.get("error");
 
+  let errorMessage = "hunter participation is at capacity.";
+  if (error === "UserNotWhitelisted") {
+    errorMessage = "Authentication failed: You are not whitelisted to play.";
+  }
+
+  console.log("error");
+  console.log(error);
+
   return (
     <main className={styles.container}>
-      <div className={styles.logo}>{error}</div>
+      <div className={styles.logo}>{errorMessage}</div>
       {/* <button className={styles.button} onClick={() => handleSignIn()}>
         Sign in
       </button> */}
