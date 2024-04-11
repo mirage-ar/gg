a0e3cfdd905bfa2b63a7ac50ccaa6068012f3b4b
fiigmnt <fiigmnt@protonmail.com>
2024-04-11 15:59:33 -0400

    bump

--- app/api/auth/login/page.tsx
+++ app/api/auth/login/page.tsx
@@ -39,6 +39,7 @@ const LoginPage = () => {
   }, [router]);
 
   useEffect(() => {
+    console.log(timeRemaining);
     const interval = setInterval(() => {
       setTimeRemaining(calculateTimeRemaining());
 
@@ -68,13 +69,13 @@ const LoginPage = () => {
     signIn("twitter", { callbackUrl: "/" });
   };
 
-  if (!isStandalone) {
-    return (
-      <main>
-        <HomeScreenOverlay />
-      </main>
-    );
-  }
+  // if (!isStandalone) {
+  //   return (
+  //     <main>
+  //       <HomeScreenOverlay />
+  //     </main>
+  //   );
+  // }
 
   if (timeRemaining > 5 * 60 * 1000) { // TODO: move this to a constant 
     return (
