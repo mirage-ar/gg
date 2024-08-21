38adabc32a9b9775d5d0cde2821df92635c204a0
fiigmnt <fiigmnt@protonmail.com>
2024-08-21 13:35:50 -0400

    testing

--- utils/constants.ts
+++ utils/constants.ts
@@ -21,6 +21,6 @@ export const GAME_DATE = {
 
 // IMPORTANT - DELETE USERS BEFORE UPDATING GAME TIME
 export const GAME_TIME = 1;
-export const GAME_LENGTH = 45 * 60 * 1000;
+export const GAME_LENGTH = 5 * 45 * 60 * 1000;
 
 export const PLAYER_COUNT = 150;
